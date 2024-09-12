c417158158150051d36284ea5b9a63187c97a428
scottgirling <156083922+scottgirling@users.noreply.github.com>
2024-09-12 17:45:21 +0100

    add new html pages & navbar, cta on index.html etc

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-let answers = document.querySelectorAll(".accordion");
-answers.forEach((event) => {
-	event.addEventListener("click", () => {
-		if (event.classList.contains("active")) {
-			event.classList.remove("active");
-		} else {
-			event.classList.add("active");
-		}
-	});
-});
-
 // MODALS ON VIEW ALL DISCOUNTS PAGE
 
 // MODAL FOR EMAIL
